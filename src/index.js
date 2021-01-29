d69627b084258d4bd531c599c076fd7308ed5286
suzygiv <suzannegivnish@gmail.com>
2021-01-29 11:55:58 -0500

    updated index.js file

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// const bodyParser = require('body-parser')
-
-// app.use(bodyParser.urlencoded({ extended: true }))
 
 
 
